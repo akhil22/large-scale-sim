580d37629bec1ba006361e8e400f9cd3dc492501
Akhil <akhil.nagariya@gmail.com>
2018-05-09 17:04:35 -0500

    added namespaces for different clients

--- Assets/Standard Assets/Vehicles/Car/Scripts/CamRos.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CamRos.cs
@@ -12,175 +12,194 @@ using ROSBridgeLib.std_msgs;
 using System.Diagnostics;
 using UnityEngine.UI;
 namespace UnityStandardAssets.Vehicles.Car{
-public class CamRos : NetworkBehaviour
-{
+  public class CamRos : NetworkBehaviour
+  {
 
-        private ROSBridgeWebSocketConnection ros = null;
-      //  ros.AddPublisher (typeof(Turtle1Teleop));
-        public void CallBackFromRos(double v, double h){
-	  UnityEngine.Debug.Log("got the callback");
-	} 
-        public Camera cam;
-	
-	[SyncVar]
-	bool start_flag = false ;
-	
-//	[SyncVar]
-	public InputField inputField;
-
-	string namsp = "no";
-	
-	public Button button;
-        CompressedImageMsg msg;
-        private int width = 800;
-        private int height = 800;
-        Texture2D texture2D; 
-        RenderTexture targetTexture;
-        void Start(){
-        ros = new ROSBridgeWebSocketConnection ("ws://localhost", 9090);
-	start_flag = false;
-        if(!isLocalPlayer){
-	   StartCoroutine(PubPosition());
-	   UnityEngine.Debug.Log("not the local player");
-           cam.enabled = false;
-           return;
-        }
-	inputField.text = "no";
-	button.onClick.AddListener(TaskOnClick);
-	StartCoroutine(UploadPNG());
-	StartCoroutine(StartRos());
-	StartCoroutine(PubOwnPosition());
-	}
-	void TaskOnClick(){
-	  if(!isLocalPlayer){
-	    return;
-	  }
-	  start_flag = true;
-	  namsp = inputField.text;
-	  UnityEngine.Debug.LogWarning("Button Pressed");
-	}
-   IEnumerator PubPosition(){
-     string pose_topic = string.Concat("/car",Process.GetCurrentProcess().Id, "/car",netId.ToString(),"/position");
-     ros.AddPublisherNew(typeof(PoseStampedMsg),pose_topic);
-     ros.Connect();
-     while(true){
-     if(!isLocalPlayer){
-    // if(start_flag){
-       UnityEngine.Debug.Log("not the local player");
-       UnityEngine.Debug.Log(inputField.text);
-       Vector3 bar = transform.position;
-       PoseStampedMsg msg = new PoseStampedMsg(new HeaderMsg(0, new TimeMsg(0,0), "cam"), new PoseMsg(new PointMsg(bar.x,bar.z,0), new QuaternionMsg(0,0,0,0)));
-       UnityEngine.Debug.Log(pose_topic);
-       ros.Publish(pose_topic, msg);
-       yield return null;
-     //  }
-     }
-     }
-   }
-   IEnumerator PubOwnPosition(){
-     string pose_topic = string.Concat("/car",Process.GetCurrentProcess().Id, "/car",netId.ToString(),"/position");
-     ros.AddPublisherNew(typeof(PoseStampedMsg),pose_topic);
-     ros.Connect();
-     while(true){
-     if(isLocalPlayer){
-    // if(start_flag){
-       UnityEngine.Debug.Log("local player");
-       UnityEngine.Debug.Log(inputField.text);
-       Vector3 bar = transform.position;
-       PoseStampedMsg msg = new PoseStampedMsg(new HeaderMsg(0, new TimeMsg(0,0), "cam"), new PoseMsg(new PointMsg(bar.x,bar.z,0), new QuaternionMsg(0,0,0,0)));
-       UnityEngine.Debug.Log(pose_topic);
-       ros.Publish(pose_topic, msg);
-       yield return null;
-     //  }
-     }
-     }
-   }
-   IEnumerator StartRos(){
-     while(!start_flag){
-       yield return null;
-     }
-//	cam.Render();
-       // targetTexture = new RenderTexture(width,height,24);
-        texture2D = new Texture2D (width, height, TextureFormat.RGB24, true);
-        targetTexture = new RenderTexture(width,height,24);
-        UnityEngine.Debug.LogWarning("started");
-       // ros = new ROSBridgeWebSocketConnection ("ws://localhost", 9090);
-        ros.AddPublisher (typeof(CompressedImageMsg));
-	ros.AddSubscriber(typeof(CmdVel));
-        ros.Connect ();
+    private ROSBridgeWebSocketConnection ros = null ;
+    //  ros.AddPublisher (typeof(Turtle1Teleop));
+    public void CallBackFromRos(double v, double h){
+      UnityEngine.Debug.Log("got the callback");
+    } 
+    IEnumerator PingPrint() {
+      // GUILayout.Label("Player ping values");
+      int i = 0;
+      UnityEngine.Debug.LogWarning(Network.connections.Length);
+      while (i < Network.connections.Length) {
+	// GUILayout.Label("Player " + Network.connections[i] + " - " + Network.GetAveragePing(Network.connections[i]) + " ms");
+	// GUILayout.Label("Player " + Network.connections[i] + " - " + Network.GetAveragePing(Network.connections[i]) + " ms");
+	i++;
 	yield return null;
+      }
+      yield return null;
+    }
+
+    public Camera cam;
+
+    //	[SyncVar]
+    bool start_flag;
+
+    //	[SyncVar]
+    public InputField inputField;
+
+    string namsp = string.Concat("car",Process.GetCurrentProcess().Id);
+    public Button button;
+    CompressedImageMsg msg;
+    private int width = 800;
+    private int height = 800;
+    Texture2D texture2D; 
+    RenderTexture targetTexture;
+    void Start(){
+      ros = new ROSBridgeWebSocketConnection ("ws://localhost", 9090);
+      start_flag = false;
+      if(!isLocalPlayer){
+	StartCoroutine(PubPosition());
+	UnityEngine.Debug.Log("not the local player");
+	cam.enabled = false;
+	return;
+      }
+      inputField.text = string.Concat("car",Process.GetCurrentProcess().Id); 
+      button.onClick.AddListener(TaskOnClick);
+      StartCoroutine(UploadPNG());
+      StartCoroutine(PingPrint());
+      StartCoroutine(StartRos());
+      StartCoroutine(PubOwnPosition());
+      //	StartCoroutine(GetDelay());
+    }
+    //  IEnumerator GetDelay(){
+    //UnityEngine.Debug.LogWarning(NetworkClient.GetRTT());
+    //	}
+    void TaskOnClick(){
+      if(!isLocalPlayer && !start_flag){
+	return;
+      }
+      start_flag = true;
+      namsp = inputField.text;
+      UnityEngine.Debug.LogWarning("Button Pressed");
+    }
+    IEnumerator PubPosition(){
+      string pose_topic = string.Concat("/car",Process.GetCurrentProcess().Id, "/car",netId.ToString(),"/position");
+      ros.AddPublisherNew(typeof(PoseStampedMsg),pose_topic);
+      ros.Connect();
+      while(true){
+	if(!isLocalPlayer){
+	  // if(start_flag){
+	  UnityEngine.Debug.Log("not the local player");
+	  UnityEngine.Debug.Log(inputField.text);
+	  Vector3 bar = transform.position;
+	  PoseStampedMsg msg = new PoseStampedMsg(new HeaderMsg(0, new TimeMsg(0,0), "cam"), new PoseMsg(new PointMsg(bar.x,bar.z,0), new QuaternionMsg(0,0,0,0)));
+	  UnityEngine.Debug.Log(pose_topic);
+	  ros.Publish(pose_topic, msg);
+//	  yield return new WaitForSeconds(0.03f);
+	  yield return null;
+	  //  }
 	}
-   IEnumerator UploadPNG(){
+      }
+    }
+    IEnumerator PubOwnPosition(){
+      string pose_topic = string.Concat("/car",Process.GetCurrentProcess().Id, "/car",netId.ToString(),"/position");
+      ros.AddPublisherNew(typeof(PoseStampedMsg),pose_topic);
+      //  ros.Connect();
+      while(true){
+	if(isLocalPlayer){
+	  // if(start_flag){
+	  UnityEngine.Debug.Log("local player");
+	  UnityEngine.Debug.Log(inputField.text);
+	  Vector3 bar = transform.position;
+	  PoseStampedMsg msg = new PoseStampedMsg(new HeaderMsg(0, new TimeMsg(0,0), "cam"), new PoseMsg(new PointMsg(bar.x,bar.z,0), new QuaternionMsg(0,0,0,0)));
+	  UnityEngine.Debug.Log(pose_topic);
+	  ros.Publish(pose_topic, msg);
+	  yield return null;
+	  //  }
+	}
+      }
+    }
+    IEnumerator StartRos(){
+      while(!start_flag){
+	yield return null;
+      }
+      //	cam.Render();
+      // targetTexture = new RenderTexture(width,height,24);
+      texture2D = new Texture2D (width, height, TextureFormat.RGB24, true);
+      targetTexture = new RenderTexture(width,height,24);
+      UnityEngine.Debug.LogWarning("started");
+      // ros = new ROSBridgeWebSocketConnection ("ws://localhost", 9090);
+      string image_topic = string.Concat(namsp,"/image/compressed");
+      ros.AddPublisherNew(typeof(CompressedImageMsg), image_topic);
+      ros.AddSubscriber(typeof(CmdVel));
+      ros.Connect ();
+      yield return null;
+    }
+    IEnumerator UploadPNG(){
       UnityEngine.Debug.LogWarning("InsideUploadPNG");
-    while (true)
-     {
-       if(!isLocalPlayer){
-	 UnityEngine.Debug.Log("not the local player");
-	 yield return null;
-       }
- //     Debug.LogWarning("InsideUploadPNG");
-        
-             yield return new WaitForEndOfFrame();
-       if(!isLocalPlayer){
-	 UnityEngine.Debug.Log("not the local player");
-	 yield return null;
-       }
-     while(!start_flag){
-       yield return null;
-     }
-             //RenderTexture targetTexture;
-//             Texture2D texture2D = new Texture2D (width, height, TextureFormat.RGB24, true);
-             cam.Render();
-             cam.targetTexture = targetTexture;
-	     Matrix4x4 m = cam.projectionMatrix;
-	     UnityEngine.Debug.Log(m);
-             yield return new WaitForEndOfFrame();
-       if(!isLocalPlayer){
-	 UnityEngine.Debug.Log("not the local player");
-	 yield return null;
-       }
-             RenderTexture.active = targetTexture;
-        //     yield return new WaitForEndOfFrame();
-             texture2D.ReadPixels (new Rect (0, 0, width, height), 0, 0);
-	   //  RenderTexture.active = null;
-             byte[] image = texture2D.EncodeToJPG ();
-          //   yield return new WaitForEndOfFrame();
-             CompressedImageMsg msg = new CompressedImageMsg(new HeaderMsg(0, new TimeMsg(0,0),"cam"), "jpeg", image);
-            // yield return new WaitForEndOfFrame();
-             //ros.Publish(CompressedImageMsg.GetMessageTopic(), msg);
-	     string image_topic = string.Concat(namsp,"/image/compressed");
-	     UnityEngine.Debug.Log(image_topic);
-             ros.Publish(image_topic, msg);
-	    // targetTexture = null;
-//	     texture2D = null;
-	     msg = null;
-	     image = null;
-            // yield return null;
-     }
- }
-  /*  void FixedUpdate(){
-   //   Debug.LogWarning("InsideUploadPNG");
-   // while (true)
+      while (true)
+      {
+	if(!isLocalPlayer){
+	  UnityEngine.Debug.Log("not the local player");
+	  yield return null;
+	}
+	//     Debug.LogWarning("InsideUploadPNG");
+
+	yield return new WaitForEndOfFrame();
+	if(!isLocalPlayer){
+	  UnityEngine.Debug.Log("not the local player");
+	  yield return null;
+	}
+	while(!start_flag){
+	  yield return null;
+	}
+	//RenderTexture targetTexture;
+	//             Texture2D texture2D = new Texture2D (width, height, TextureFormat.RGB24, true);
+	cam.Render();
+	cam.targetTexture = targetTexture;
+	Matrix4x4 m = cam.projectionMatrix;
+	UnityEngine.Debug.Log(m);
+	yield return new WaitForEndOfFrame();
+	if(!isLocalPlayer){
+	  UnityEngine.Debug.Log("not the local player");
+	  yield return null;
+	}
+	RenderTexture.active = targetTexture;
+	//     yield return new WaitForEndOfFrame();
+	texture2D.ReadPixels (new Rect (0, 0, width, height), 0, 0);
+	//  RenderTexture.active = null;
+	byte[] image = texture2D.EncodeToJPG ();
+	//   yield return new WaitForEndOfFrame();
+	CompressedImageMsg msg = new CompressedImageMsg(new HeaderMsg(0, new TimeMsg(0,0),"cam"), "jpeg", image);
+	// yield return new WaitForEndOfFrame();
+	//ros.Publish(CompressedImageMsg.GetMessageTopic(), msg);
+	string image_topic = string.Concat(namsp,"/image/compressed");
+	UnityEngine.Debug.Log(image_topic);
+	ros.Publish(image_topic, msg);
+	// targetTexture = null;
+	//	     texture2D = null;
+	msg = null;
+	image = null;
+	// yield return null;
+      }
+    }
+    /*  void FixedUpdate(){
+    //   Debug.LogWarning("InsideUploadPNG");
+    // while (true)
     // {
-        
-      //       yield return new WaitForEndOfFrame();
-             //RenderTexture targetTexture;
-             Texture2D texture2D = new Texture2D (width, height, TextureFormat.RGB24, true);
-             RenderTexture targetTexture;
-             targetTexture = new RenderTexture(width,height,24);
-             cam.targetTexture = targetTexture;
-             cam.Render();
-        //     yield return new WaitForEndOfFrame();
-             RenderTexture.active = targetTexture;
-        //     yield return new WaitForEndOfFrame();
-             texture2D.ReadPixels (new Rect (0, 0, width, height), 0, 0);
-             byte[] image = texture2D.EncodeToJPG ();
-          //   yield return new WaitForEndOfFrame();
-             CompressedImageMsg msg = new CompressedImageMsg(new HeaderMsg(0, new TimeMsg(0,0),"cam"), "jpeg", image);
-            // yield return new WaitForEndOfFrame();
-             ros.Publish(CompressedImageMsg.GetMessageTopic(), msg);
-            // yield return null;
-  //   }
- }*/
-}
+
+    //       yield return new WaitForEndOfFrame();
+    //RenderTexture targetTexture;
+    Texture2D texture2D = new Texture2D (width, height, TextureFormat.RGB24, true);
+    RenderTexture targetTexture;
+    targetTexture = new RenderTexture(width,height,24);
+    cam.targetTexture = targetTexture;
+    cam.Render();
+    //     yield return new WaitForEndOfFrame();
+    RenderTexture.active = targetTexture;
+    //     yield return new WaitForEndOfFrame();
+    texture2D.ReadPixels (new Rect (0, 0, width, height), 0, 0);
+    byte[] image = texture2D.EncodeToJPG ();
+    //   yield return new WaitForEndOfFrame();
+    CompressedImageMsg msg = new CompressedImageMsg(new HeaderMsg(0, new TimeMsg(0,0),"cam"), "jpeg", image);
+    // yield return new WaitForEndOfFrame();
+    ros.Publish(CompressedImageMsg.GetMessageTopic(), msg);
+    // yield return null;
+    //   }
+    }*/
+  }
 }
--- Assets/Standard Assets/Vehicles/Car/Scripts/CmdVel.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CmdVel.cs
@@ -6,6 +6,7 @@ using System;
 using SimpleJSON;
 using ROSBridgeLib.geometry_msgs;
 using UnityEngine.UI;
+using System.Diagnostics;
 namespace UnityStandardAssets.Vehicles.Car{
     [RequireComponent(typeof(CarController))]
     public class CmdVel : ROSBridgeSubscriber{
@@ -13,7 +14,7 @@ namespace UnityStandardAssets.Vehicles.Car{
       public InputField inputField;
   //    public Button button;
 
-      string namsp = "no";
+      string namsp = string.Concat("no",Process.GetCurrentProcess().Id);
       static bool start_flag = false;
       public static string command_topic;
       public static bool msg_flag;
@@ -21,7 +22,7 @@ namespace UnityStandardAssets.Vehicles.Car{
  //     GameObject car;
       //s = new Steering();
       public static string GetMessageTopic(){
-	Debug.LogWarning(command_topic);
+	UnityEngine.Debug.LogWarning(command_topic);
 	start_flag = true;
 	return command_topic;
       }
@@ -29,7 +30,7 @@ namespace UnityStandardAssets.Vehicles.Car{
 	if(!isLocalPlayer){
 	  return;
 	}
-	inputField.text = "no";
+	inputField.text = string.Concat("no",Process.GetCurrentProcess().Id);
 //	button.onClick.AddListener(TaskOnClick);
 	msg_flag = false;
         m_Car = GetComponent<CarController>();
@@ -50,7 +51,7 @@ namespace UnityStandardAssets.Vehicles.Car{
         msg_flag = true;
 	in_msg = msg;
 //	Application.Quit();
-        Debug.Log("Message Recieved");
+        UnityEngine.Debug.Log("Message Recieved");
 	// s.UpdateCmdVel(cmdvel.linear.x, cmdvel.angular.z);
       }
       void FixedUpdate(){
@@ -64,7 +65,8 @@ namespace UnityStandardAssets.Vehicles.Car{
 	  return;
 	}
 	TwistMsg cmdvel = (TwistMsg) in_msg;
-	m_Car.Move((float)(-1*cmdvel.GetAngular().GetZ()), (float)(cmdvel.GetLinear().GetX()),(float)(cmdvel.GetLinear().GetX()), 0f);
+	//m_Car.Move((float)(-1*cmdvel.GetAngular().GetZ()), (float)(cmdvel.GetLinear().GetX()),(float)(cmdvel.GetLinear().GetX()), 0f);
+	m_Car.Move((float)(-1*cmdvel.GetAngular().GetZ()), 0.3f,0.3f, 0f);
 //	m_Car.Move((float)(cmdvel.GetAngular().GetZ()),(float)0.1 ,(float)0.1, 0f);
       }
     }
